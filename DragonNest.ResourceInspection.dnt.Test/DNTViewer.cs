d5f135f74d8658b406bfae41f740a19d1edcce70
Joseph-plex <joseph@plexxis.com>
2014-05-13 06:03:38 -0400

    Change log
    - Name Changes

--- DragonNest.ResourceInspection.dnt.Test/DNTViewer.cs
+++ DragonNest.ResourceInspection.dnt.Test/DNTViewer.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
 namespace DragonNest.ResourceInspection.dnt.Test
 {
     public partial class DNTViewer : Form

ac8d947c6d7cb96f074503e126d4f41a7cc33e89
Joseph-plex <joseph@plexxis.com>
2014-06-09 16:48:53 -0400

    Added Graphical Elements to the PakViewer.

--- DragonNest.Dnt.Test/Pak.Viewer/PakViewer.Designer.cs
+++ DragonNest.Dnt.Test/Pak.Viewer/PakViewer.Designer.cs
@@ -28,19 +28,164 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.naviBar1 = new Guifreaks.NavigationBar.NaviBar(this.components);
+            this.naviBand1 = new Guifreaks.NavigationBar.NaviBand(this.components);
+            this.PakTree = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.naviBar1)).BeginInit();
+            this.naviBar1.SuspendLayout();
+            this.naviBand1.ClientArea.SuspendLayout();
+            this.naviBand1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.naviBar1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listView1);
+            this.splitContainer1.Panel2.Controls.Add(this.statusStrip1);
+            this.splitContainer1.Panel2.Controls.Add(this.toolStrip1);
+            this.splitContainer1.Size = new System.Drawing.Size(995, 505);
+            this.splitContainer1.SplitterDistance = 331;
+            this.splitContainer1.TabIndex = 0;
+            this.splitContainer1.SplitterMoving += new System.Windows.Forms.SplitterCancelEventHandler(this.splitContainer1_SplitterMoving);
+            // 
+            // naviBar1
+            // 
+            this.naviBar1.ActiveBand = this.naviBand1;
+            this.naviBar1.Controls.Add(this.naviBand1);
+            this.naviBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.naviBar1.Location = new System.Drawing.Point(0, 0);
+            this.naviBar1.Name = "naviBar1";
+            this.naviBar1.Size = new System.Drawing.Size(331, 505);
+            this.naviBar1.TabIndex = 0;
+            this.naviBar1.Text = "Pak Explorer";
+            this.naviBar1.Resize += new System.EventHandler(this.naviBar1_Resize);
+            // 
+            // naviBand1
+            // 
+            // 
+            // naviBand1.ClientArea
+            // 
+            this.naviBand1.ClientArea.Controls.Add(this.PakTree);
+            this.naviBand1.ClientArea.Location = new System.Drawing.Point(0, 0);
+            this.naviBand1.ClientArea.Name = "ClientArea";
+            this.naviBand1.ClientArea.Size = new System.Drawing.Size(329, 438);
+            this.naviBand1.ClientArea.TabIndex = 0;
+            this.naviBand1.ClientArea.Text = "Pak Explorer";
+            this.naviBand1.Location = new System.Drawing.Point(1, 27);
+            this.naviBand1.Name = "naviBand1";
+            this.naviBand1.Size = new System.Drawing.Size(329, 438);
+            this.naviBand1.TabIndex = 3;
+            this.naviBand1.Text = "Pak Explorer";
+            // 
+            // PakTree
+            // 
+            this.PakTree.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PakTree.Location = new System.Drawing.Point(0, 0);
+            this.PakTree.Name = "PakTree";
+            this.PakTree.Size = new System.Drawing.Size(329, 438);
+            this.PakTree.TabIndex = 0;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Location = new System.Drawing.Point(0, 25);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(660, 458);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 422;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Original Size";
+            this.columnHeader2.Width = 112;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Compressed Size";
+            this.columnHeader3.Width = 121;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 483);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(660, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(660, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
             // 
             // PakViewer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(995, 505);
+            this.Controls.Add(this.splitContainer1);
             this.Name = "PakViewer";
             this.Text = "PakViewer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.PakViewer_FormClosing);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.naviBar1)).EndInit();
+            this.naviBar1.ResumeLayout(false);
+            this.naviBand1.ClientArea.ResumeLayout(false);
+            this.naviBand1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private Guifreaks.NavigationBar.NaviBar naviBar1;
+        private Guifreaks.NavigationBar.NaviBand naviBand1;
+        private System.Windows.Forms.TreeView PakTree;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
     }
 }
--- DragonNest.Dnt.Test/Pak.Viewer/PakViewer.cs
+++ DragonNest.Dnt.Test/Pak.Viewer/PakViewer.cs
@@ -7,14 +7,83 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using DragonNest.ResourceInspection.Pak;
+using DragonNest.ResourceInspector.Pak;
 
+using Guifreaks.NavigationBar;
 namespace DragonNest.ResourceInspection.Pak.Viewer
 {
     public partial class PakViewer : Form
     {
+        Stream pakStream;
+        PakFile pakFile;
         public PakViewer()
         {
             InitializeComponent();
         }
+
+        public void LoadPakStream(Stream stream)
+        {
+            if (pakStream != null)  pakStream.Close();
+            pakFile = new PakFile(pakStream = stream);
+
+            RefreshPakTree();
+        }
+
+        void RefreshPakTree()
+        {
+            //To stop graphical inconsistency
+            PakTree.SuspendLayout();
+
+            PakTree.Nodes.Clear();
+            foreach(var file in pakFile.Files)
+            {
+                var pathComponents = file.FileName.Split(new char [] {'\\'},StringSplitOptions.RemoveEmptyEntries);
+                var Nodes = PakTree.Nodes;
+                foreach(var v in pathComponents)
+                {
+                    if (!Nodes.ContainsKey(v))
+                        Nodes.Add(new TreeNode(v) { Name = v });
+                    var next = Nodes.Find(v, false).First();
+                    Nodes = next.Nodes;
+                }
+            }
+
+            //To update the Graphics
+            PakTree.ResumeLayout();
+        }
+
+        void RefreshDetailView()
+        {
+
+        }
+
+        private void naviBar1_Resize(object sender, EventArgs e)
+        {
+            var obj = ((NaviBar)sender);
+            if (obj.Collapsed)
+            {
+                splitContainer1.SplitterDistance = obj.Size.Width;
+                splitContainer1.IsSplitterFixed = true;
+            }
+            else
+            {
+                splitContainer1.IsSplitterFixed = false;
+                splitContainer1.SplitterDistance = obj.Size.Width;
+            }
+        }
+        private void splitContainer1_SplitterMoving(object sender, SplitterCancelEventArgs e)
+        {
+            naviBar1.Width = splitContainer1.SplitterDistance;
+        }
+
+        private void PakViewer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (pakStream != null)
+                pakStream.Close();
+        }
+
+      
     }
 }

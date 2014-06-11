1d5ef86000fff37c466a28c60b77358711f3ef12
Joseph-plex <joseph@plexxis.com>
2014-06-11 14:42:31 -0400

    Changed the streams that the program handles to handle file streams only.

--- DragonNest.ResourceInspector.Pak/PakFile.cs
+++ DragonNest.ResourceInspector.Pak/PakFile.cs
@@ -18,29 +18,34 @@ namespace DragonNest.ResourceInspection.Pak
             Files = new List<FileHeader>();
         }
 
-        public PakFile(Stream stream) : this()
+        public PakFile(FileStream stream) : this()
         {
             //If we can't seek, we can't parse the stream
-            if (!stream.CanSeek)
-                throw new Exception("Stream does not support seeking");
             
             //We don't own the stream, so leave open is set to true
-            using(var reader = new BinaryReader(stream,Encoding.Default, true))
-            {
-                //If we don't have the signature then that means we are dealing with an unknown file type
-                if (PakHeader.Identifier != Encoding.ASCII.GetString(reader.ReadBytes(0x20)))
-                    throw new Exception("Invalid File Format");
+            try { 
+                using(var reader = new BinaryReader(stream,Encoding.Default, true))
+                {
+                    //If we don't have the signature then that means we are dealing with an unknown file type
+                    if (PakHeader.Identifier != Encoding.ASCII.GetString(reader.ReadBytes(0x20)))
+                        throw new Exception("Invalid File Format");
 
-                //This is where the FileCount and File Offset are stored.
-                stream.Position = 0x104L;
-                Header.FileCount = reader.ReadUInt32();
-                Header.TableOffset = reader.ReadUInt32();
+                    //This is where the FileCount and File Offset are stored.
+                    stream.Position = 0x104L;
+                    Header.FileCount = reader.ReadUInt32();
+                    Header.TableOffset = reader.ReadUInt32();
 
-                //We'll begin reading our file headers at the first offset Position
-                stream.Position = Header.TableOffset;
-                for (int i = 0; i < Header.FileCount; i++)
-                    Files.Add(FileHeader.FromBinaryReader(reader));
+                    //We'll begin reading our file headers at the first offset Position
+                    stream.Position = Header.TableOffset;
+                    for (int i = 0; i < Header.FileCount; i++)
+                        Files.Add(FileHeader.FromBinaryReader(reader));
+                }
             }
+            catch(Exception e)
+            { 
+
+            }
+
         }
     }
 }

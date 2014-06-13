8e68d1ed4755992fd7dd9825004f6efadca14c20
Joseph-plex <joseph@plexxis.com>
2014-06-13 11:05:31 -0400

    Added Region to code

--- DragonNest.ResourceInspector/Pak.Viewer/PakViewer.cs
+++ DragonNest.ResourceInspector/Pak.Viewer/PakViewer.cs
@@ -41,11 +41,7 @@ namespace DragonNest.ResourceInspector.Pak.Viewer
             InitializeComponent();
         }
 
-        private void PakViewer_Load(object sender, EventArgs e)
-        {
-            toolStripTextBox1.Width = toolStrip1.Size.Width - 4;
-            toolStrip1.SizeChanged += (s, a) => toolStripTextBox1.Width = toolStrip1.Size.Width - 4;
-        }
+     
         public void LoadPakStream(Stream stream) 
         {
             Status = 0;
@@ -96,53 +92,13 @@ namespace DragonNest.ResourceInspector.Pak.Viewer
             PakTree.ResumeLayout();
         }
 
-        private void naviBar1_Resize(object sender, EventArgs e)
-        {
-            var obj = ((NaviBar)sender);
-            if (obj.Collapsed)
-            {
-                splitContainer1.SplitterDistance = obj.Size.Width;
-                splitContainer1.IsSplitterFixed = true;
-            }
-            else
-            {
-                splitContainer1.IsSplitterFixed = false;
-                splitContainer1.SplitterDistance = obj.Size.Width;
-            }
-        }
-        private void splitContainer1_SplitterMoving(object sender, SplitterCancelEventArgs e)
-        {
-            naviBar1.Width = splitContainer1.SplitterDistance;
-        }
 
-        private void PakViewer_FormClosing(object sender, FormClosingEventArgs e)
+        #region EventHandlers
+        private void PakViewer_Load(object sender, EventArgs e)
         {
-            if (pakStream != null)
-                pakStream.Close();
+            toolStripTextBox1.Width = toolStrip1.Size.Width - 4;
+            toolStrip1.SizeChanged += (s, a) => toolStripTextBox1.Width = toolStrip1.Size.Width - 4;
         }
-
-        private void PakTree_AfterSelect(object sender, TreeViewEventArgs e)
-        {
-            if (e.Node.Nodes.Count == 0)
-                return;
-
-            listView1.Items.Clear();
-            foreach (TreeNode node in e.Node.Nodes)
-            {
-                ListViewItem item = new ListViewItem(node.Name);
-                listView1.Items.Add(item);
-            }
-            
-            var path = String.Empty;
-            var Node = e.Node;
-            for (int i = 0; i <= e.Node.Level; i++, path = path.Insert(0,@"\"))
-            {
-                path = path.Insert(0, Node.Text);
-                Node = Node.Parent;
-            }
-            toolStripTextBox1.Text = path;
-        }
-
         private void listView1_DoubleClick(object sender, EventArgs e)
         {
             if (sender == listView1)
@@ -179,26 +135,25 @@ namespace DragonNest.ResourceInspector.Pak.Viewer
 
         private void listView1_MouseClick(object sender, MouseEventArgs e)
         {
-            switch(e.Button)
+            switch (e.Button)
             {
                 case MouseButtons.Right:
-                    if(listView1.SelectedItems.Count == 0)
+                    if (listView1.SelectedItems.Count == 0)
                         return;
                     var Nodes = PakTree.Nodes;
                     var path = toolStripTextBox1.Text + @"\" + listView1.SelectedItems[0].Text;
                     path = path.Trim('\0');
                     var pathComponents = path.Split(new char[] { '\\' }, StringSplitOptions.RemoveEmptyEntries);
-                    
-                    foreach(var v in pathComponents)
+
+                    foreach (var v in pathComponents)
                         Nodes = Nodes.Find(v, false).First().Nodes;
 
-                    if(listView1.SelectedItems[0].Text.EndsWith(".dnt"))
+                    if (listView1.SelectedItems[0].Text.EndsWith(".dnt"))
                         contextMenuStrip1.Show(e.X, e.Y);
-                    
+
                     break;
             }
         }
-
         private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
         {
         }
@@ -221,6 +176,52 @@ namespace DragonNest.ResourceInspector.Pak.Viewer
             }
         }
 
-     
+        private void naviBar1_Resize(object sender, EventArgs e)
+        {
+            var obj = ((NaviBar)sender);
+            if (obj.Collapsed)
+            {
+                splitContainer1.SplitterDistance = obj.Size.Width;
+                splitContainer1.IsSplitterFixed = true;
+            }
+            else
+            {
+                splitContainer1.IsSplitterFixed = false;
+                splitContainer1.SplitterDistance = obj.Size.Width;
+            }
+        }
+        private void splitContainer1_SplitterMoving(object sender, SplitterCancelEventArgs e)
+        {
+            naviBar1.Width = splitContainer1.SplitterDistance;
+        }
+
+        private void PakViewer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (pakStream != null)
+                pakStream.Close();
+        }
+
+        private void PakTree_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node.Nodes.Count == 0)
+                return;
+
+            listView1.Items.Clear();
+            foreach (TreeNode node in e.Node.Nodes)
+            {
+                ListViewItem item = new ListViewItem(node.Name);
+                listView1.Items.Add(item);
+            }
+
+            var path = String.Empty;
+            var Node = e.Node;
+            for (int i = 0; i <= e.Node.Level; i++, path = path.Insert(0, @"\"))
+            {
+                path = path.Insert(0, Node.Text);
+                Node = Node.Parent;
+            }
+            toolStripTextBox1.Text = path;
+        }
+        #endregion
     }
 }
